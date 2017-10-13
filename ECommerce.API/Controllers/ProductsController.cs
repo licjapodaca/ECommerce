9d84ea16c0040d0a98aa1f4ed7977c7b5bfef1b4
M.C.S.D. Jorge Apodaca <japodaca@bts.com.mx>
2017-10-12 17:23:37 -0700

    CancellationToken sample

--- ECommerce.API/Controllers/ProductsController.cs
+++ ECommerce.API/Controllers/ProductsController.cs
@@ -7,49 +7,65 @@ using ECommerce.API.Model;
 using ECommerce.ProductCatalog.Model;
 using Microsoft.ServiceFabric.Services.Remoting.Client;
 using Microsoft.ServiceFabric.Services.Client;
+using Microsoft.Extensions.Logging;
+using System.Threading;
 
 namespace ECommerce.API.Controllers
 {
-    [Route("api/[controller]")]
-    public class ProductsController : Controller
-    {
-        private readonly IProductCatalogService _catalogService;
+	[Route("api/[controller]")]
+	public class ProductsController : Controller
+	{
+		private readonly IProductCatalogService _catalogService;
+		private readonly ILogger _logger;
 
-        public ProductsController()
-        {
-            _catalogService = ServiceProxy.Create<IProductCatalogService>(
-                new Uri("fabric:/ECommerce/ProductCatalog"),
-                new ServicePartitionKey(0));
-        }
+		public ProductsController(ILogger<ProductsController> logger)
+		{
+			_logger = logger;
 
-        [HttpGet]
-        public async Task<IEnumerable<ApiProduct>> Get()
-        {
-            IEnumerable<Product> allProducts = await _catalogService.GetAllProducts();
+			_catalogService = ServiceProxy.Create<IProductCatalogService>(
+				new Uri("fabric:/ECommerce/ProductCatalog"),
+				new ServicePartitionKey(0));
+		}
 
-            return allProducts.Select(p => new ApiProduct
-            {
-                Id = p.Id,
-                Name = p.Name,
-                Description = p.Description,
-                Price = p.Price,
-                IsAvailable = p.Availability > 0
-            });
-        }
+		[HttpGet]
+		public async Task<IEnumerable<ApiProduct>> Get(CancellationToken cancellationToken)
+		{
+			IEnumerable<Product> allProducts = await _catalogService.GetAllProducts();
 
-        [HttpPost]
-        public async Task Post([FromBody] ApiProduct product)
-        {
-            var newProduct = new Product()
-            {
-                Id = Guid.NewGuid(),
-                Name = product.Name,
-                Description = product.Description,
-                Price = product.Price,
-                Availability = 100
-            };
+			_logger.LogInformation("Start long process...");
 
-            await _catalogService.AddProduct(newProduct);
-        }
-    }
+			//for (var i = 0; i < 10; i++)
+			//{
+			cancellationToken.ThrowIfCancellationRequested();
+			//	// slow non-cancellable work
+			await Task.Delay(10_000);
+			//}
+
+			_logger.LogInformation("Finished long process...");
+
+			return allProducts.Select(p => new ApiProduct
+			{
+				Id = p.Id,
+				Name = p.Name,
+				Description = p.Description,
+				Price = p.Price,
+				IsAvailable = p.Availability > 0
+			});
+		}
+
+		[HttpPost]
+		public async Task Post([FromBody] ApiProduct product)
+		{
+			var newProduct = new Product()
+			{
+				Id = Guid.NewGuid(),
+				Name = product.Name,
+				Description = product.Description,
+				Price = product.Price,
+				Availability = 100
+			};
+
+			await _catalogService.AddProduct(newProduct);
+		}
+	}
 }
